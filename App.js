0149f6893fa81ba1a3b68b3455a8560545a1dad5
Lasse Haverinen <lasse.haverinen@gmail.com>
2020-02-07 11:54:19 +0200

    Added state, text input and style examples

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import HelloWorld from './components/HelloWorld';
-import CustomText from './components/CustomText'; 
+import HelloWorld from './tutorials/step1-helloworld/HelloWorld';
+import CustomText from './tutorials/step2-props/CustomText';
+import Greeter from './tutorials/step3-state-and-text-input/Greeter'
+import StyleDemo from './tutorials/step4-style/StyleDemo'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <HelloWorld></HelloWorld>
       <CustomText value="This is data passed by props to the component"></CustomText>
+      <Greeter></Greeter>
+      <StyleDemo></StyleDemo>
     </View>
   );
 }
