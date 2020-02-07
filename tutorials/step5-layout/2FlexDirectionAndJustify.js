8e5011f3e35fc30523d49beec9041053f6ccfc4c
Lasse Haverinen <lasse.haverinen@gmail.com>
2020-02-07 15:46:29 +0200

    Added comments

--- tutorials/step5-layout/2FlexDirectionAndJustify.js
+++ tutorials/step5-layout/2FlexDirectionAndJustify.js
@@ -7,8 +7,9 @@ export default class FlexDirectionAndJustify extends Component {
       <View style={{
         flex: 1,
         paddingTop: 20,
-        flexDirection: 'column',
-        justifyContent: 'space-around' /* Test out all the values */
+        flexDirection: 'column', // MAIN AXIS
+        justifyContent: 'space-around', // MAIN AXIS - Test out all the values
+        alignItems: 'baseline' // CROSS AXIS
       }}>
         <Text style={{ height: 80, borderWidth: 1, borderColor: 'black' }}>
           justifyContent does not really work directly with flex dimensioned elements only
